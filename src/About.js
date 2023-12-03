a92be8d64de9353607a22a3d8d5cf36c6e21a2f1
ajithkumar320 <ajithmeow3037@gmail.com>
2023-12-03 12:52:10 +0530

    update

---
+++ src/About.js
@@ -0,0 +1,146 @@
+
+
+
+function  About(){
+    return(
+    <div>
+		<section className ="header">
+			<div className="container">
+				<div className="row align-items"> 
+					<div className="col_2">
+						<div className="content">
+							<h2>All Stores</h2>
+						</div>
+					</div>	
+					<div className="col_10">
+						<div className="nav_item">
+							<nav className="navbar">
+								<ul className="list">
+									<li><a href="#">Home</a></li>
+									<li><a href="#about">About</a></li>
+									<li><a href="#foods">Mobiles</a></li>
+									<li><a href="#Inf">Tvs</a></li>
+									<li><a href="#wf">Watchs</a></li>
+									<li><a href="#con">Contact</a></li>
+									<li><a className="button" href="#about">Get Started</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div> 
+		</section>
+		
+		<section className="banner">
+			<div className="container">
+				<div className="row">
+					<div className="col_12">
+						<div className="cont">
+							 <img src="images/stock-vector-illustratio.jpg" />  
+						</div>
+					</div>
+				</div> 
+			</div>		
+		</section>
+		
+		<section className="sec-1">
+			<div className="container">
+				<div className="row">
+					<div className="col_12">
+						<div className="centent">
+							<h4>MOBILES</h4>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</section>
+		
+		<section className="sec-2">
+			<div className="container">
+				<div className="row">	
+					<div className="col_2">
+						<div className="cont1">
+							<img src="images/iqoo-z7s-5g.webp" />
+						</div>
+						<div className="para">
+							<h3>Rs.20000/-</h3>
+							<p>iqoo-z7s-5g</p>
+						</div>
+						<div className="bttn">
+							<a href="file:///E:/fabevy/task23/index.html">Read More</a>
+						</div>
+					</div>
+					
+					<div className="col_2">
+						<div className="cont1">
+							<img src="images/OnePlus Nord CE3.webp" />
+						</div>
+						<div className="para">
+							<h3>Rs.19500/-</h3>
+							<p>OnePlus Nord CE3</p>
+						</div>
+						<div className="bttn">
+							<a href="#">Read More</a>
+						</div>
+					</div>
+					
+					<div className="col_2">
+						<div className="cont1">
+							<img src="images/realme narzo n53.webp" />
+						</div>
+						<div className="para">
+							<h3>Rs.16000/-</h3>
+							<p>realme narzo n53</p>
+						</div>
+						<div className="bttn">
+							<a href="#">Read More</a>
+						</div>
+					</div>
+					
+					<div className="col_2">
+						<div className="cont1">
+							<img src="images/vivo-t2-5g-2022.webp" />
+						</div>
+						<div className="para">
+							<h3>Rs.15850/-</h3>
+							<p>vivo-t2-5g</p>
+						</div>
+						<div className="bttn">
+							<a href="#">Read More</a>
+						</div>
+					</div>
+					
+					<div className="col_2">
+						<div className="cont1">
+							<img src="images/samsung-galaxy-a83-5.webp" />
+						</div>
+						<div className="para">
+							<h3>Rs.20180/-</h3>
+							<p>samsung-galaxy-s21</p>
+						</div>
+						<div className="bttn">
+							<a href="#">Read More</a>
+						</div>
+					</div>
+					
+					<div className="col_2">
+						<div className="cont1">
+							<img src="images/Lava Blaze2.webp" />
+						</div>
+						<div className="para">
+							<h3>Rs.18170/-</h3>
+							<p>Lava Blaze2</p>
+						</div>
+						<div className="bttn">
+							<a href="#">Read More</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+    )
+}
+
+
+export default About
